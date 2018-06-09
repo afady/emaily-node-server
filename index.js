230796ca47879cd347963c7f6ed80fca817749d4
ff55@gmail.com <ff55@gmail.com>
2018-06-09 09:42:02 -0400

    auth complete

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+// https://whispering-retreat-42977.herokuapp.com/
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
