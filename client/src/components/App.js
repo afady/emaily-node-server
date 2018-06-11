493018ecb7e9b6986916238986d41a679df31d14
afady <fadyfarid55@gmail.com>
2018-06-10 22:21:28 -0400

    login/logout frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const App = () => {
-  return <div>Hello!</div>;
-};
+import Header from "./Header";
 
-export default App;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Header logout={this.props.logout} />
+            <div className="container">
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/surveys" component={Dashboard} />
+              <Route exact path="/surveys/new" component={SurveyNew} />
+            </div>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
