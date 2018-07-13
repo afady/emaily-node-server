9c8b763ce0aeff2b67ee3b8593e3795d3535fe21
afady <fadyfarid55@gmail.com>
2018-07-13 11:05:46 -0400

    add handleToken action, start billingRoutes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,6 +10,12 @@ export const fetchUser = () => async dispatch => {
   });
 };
 
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+
+  dispatch({ type: FETCH_USER, payload: res.data});
+};
+
 export const logout = () => async dispatch => {
   const res = await axios.get("/api/logout");
   await dispatch({
