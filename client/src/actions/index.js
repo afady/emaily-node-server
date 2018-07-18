5403f8f7a377cc18f009e0b5a1a4f81cfa679e27
afady <fadyfarid55@gmail.com>
2018-07-18 13:12:25 -0400

    finish billing

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,11 @@
 import axios from "axios";
-import { push } from "connected-react-router";
-import { FETCH_USER, LOGOUT } from "./types";
+import {
+  push
+} from "connected-react-router";
+import {
+  FETCH_USER,
+  LOGOUT
+} from "./types";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
@@ -13,7 +18,10 @@ export const fetchUser = () => async dispatch => {
 export const handleToken = (token) => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
-  dispatch({ type: FETCH_USER, payload: res.data});
+  dispatch({
+    type: FETCH_USER,
+    payload: res.data
+  });
 };
 
 export const logout = () => async dispatch => {
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
