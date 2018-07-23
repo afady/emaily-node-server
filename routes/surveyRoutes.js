3f09176cab6449895ea3613ec777120694a953a4
afady <fadyfarid55@gmail.com>
2018-07-23 18:53:45 -0400

    sendgrid boilerplate

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -3,6 +3,22 @@ const requireCredits = require('../middlewares/requireCredits');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+        const {
+            title,
+            subject,
+            body,
+            recipients
+        } = req.body;
 
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => {
+                email.trim()
+            }),
+            _user: req.user.id,
+            dateSent: Date.now()
+        })
     });
 };
